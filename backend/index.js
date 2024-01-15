2d042758f232373c2fc57ed13d4098141af6d135
SandeshHegde <119592672+SandeshHegde45@users.noreply.github.com>
2024-01-15 18:07:10 +0530

    CORS And URL fixed

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 5000;
 const mongoDB = require("./db");
 const cors = require("cors");
-app.use(cors());
+app.use(cors({
+  origin: ["https://foody-frontend-jade.vercel.app"],
+  methods: ["POST", "GET"],
+  credentials: true,
+}));
 
 mongoDB();
 
@@ -13,7 +17,7 @@ app.get("/", (req, res) => {
 app.use(express.json());
 app.use('/api', require('./Routes/CreateUser'));
 app.use('/api', require('./Routes/DisplayData'));
-app.use("/api", require("./Routes/OrderData")); 
+app.use("/api", require("./Routes/OrderData"));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);

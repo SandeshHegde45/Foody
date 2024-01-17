8072c5b2468f39d6dfdba5b7e068ed393cb44d0d
SandeshHegde <119592672+SandeshHegde45@users.noreply.github.com>
2024-01-17 13:18:14 +0530

    Backend Updated

--- backend/db.js
+++ backend/db.js
@@ -1,35 +1,21 @@
 const mongoose = require("mongoose");
 
-const mongoURI =
-  "mongodb://Foody:foody123@ac-yyc1lmm-shard-00-00.3zkgdab.mongodb.net:27017,ac-yyc1lmm-shard-00-01.3zkgdab.mongodb.net:27017,ac-yyc1lmm-shard-00-02.3zkgdab.mongodb.net:27017/foody?ssl=true&replicaSet=atlas-50soql-shard-0&authSource=admin&retryWrites=true&w=majority";
+const mongoURI = "mongodb+srv://Foody:foody123@cluster1.3zkgdab.mongodb.net/foody?retryWrites=true&w=majority"
 
-const mongoDB = async () => {
-  await mongoose.connect(
-    mongoURI,
-    { useNewUrlParser: true },
-    async (err, result) => {
-      if (err) {
-        console.log("Error connecting to MongoDB");
-      } else {
+const MongoDB = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+
         console.log("Connected to MongoDB");
-        const fetched_data = await mongoose.connection.db.collection(
-          "food_items"
-        );
-        fetched_data.find({}).toArray(async function (err, data) {
-          const foodCategory = await mongoose.connection.db.collection(
-            "foodCategory"
-          );
-          foodCategory.find({}).toArray(function (err, catData) {
-            if (err) console.log(err);
-            else {
-              global.food_items = data;
-              global.foodCategory = catData;
-            }
-          });
-        });
-      }
+
+        const fetchedData = await mongoose.connection.db.collection("food_items").find({}).toArray();
+        const foodCategoryData = await mongoose.connection.db.collection("foodCategory").find({}).toArray();
+
+        global.food_items = fetchedData;
+        global.foodCategory = foodCategoryData;
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err.message);
     }
-  );
 };
 
-module.exports = mongoDB;
+module.exports = MongoDB;
--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 5000;
-const mongoDB = require("./db");
-const cors = require("cors");
+const MongoDB = require('./db');
+
+const port = 5050;
+
 app.use(cors({
-  origin: ["https://foody-frontend-two.vercel.app"],
-  methods: ["POST", "GET"],
-  credentials: true,
+    origin: ["https://foody-frontend-two.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
 }));
 
-mongoDB();
+MongoDB();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
 app.use(express.json());
 app.use('/api', require('./Routes/CreateUser'));
 app.use('/api', require('./Routes/DisplayData'));
 app.use("/api", require("./Routes/OrderData"));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log(`Server is running on http://localhost:${port}`);
+})
